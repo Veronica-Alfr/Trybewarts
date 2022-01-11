6159494deed4dbbe86d3fa5251eaa5ab8eaa6f70
lazaro <lazaroandriola@gmail.com>
2022-01-11 16:08:39 -0300

    Requisitos 3 e 4 concluido

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function verificaLogin() {
   const email = document.getElementById('emailLogin');
   const senha = document.getElementById('senhaLogin');
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-    alert('Olá, Tryber');
+    alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos');
   }
