9141554a6e74144a9dc8eb15686bda630c67d441
Veronica-Alfr <veagalves@gmail.com>
2022-01-11 21:55:09 -0300

    Requisito 14 JS concluído

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function verificaLogin() {
 }
 
 botaoLogin.addEventListener('click', verificaLogin);
+
+const divAvaliattion = document.getElementById('avaliation-container');
+
+// Ajuda de Raphael Martins
+function radio() {
+  for (let i = 1; i < 11; i += 1) {
+    const label = document.createElement('label');
+    const radiosAvaliation = document.createElement('input');
+    radiosAvaliation.type = 'radio';
+    radiosAvaliation.value = i;
+    radiosAvaliation.name = 'rate';
+    radiosAvaliation.id = `rate-${i}`;
+    label.for = radiosAvaliation.id;
+    label.innerHTML = i;
+    label.appendChild(radiosAvaliation);
+    divAvaliattion.appendChild(label);
+  }
+}
+radio();
